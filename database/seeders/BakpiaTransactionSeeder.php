dcec653154fc83692fc44a0f5ff43150e55ba017
alvinChristianto <alvinchristianto93@gmail.com>
2025-04-11 09:40:09 +0700

    adding seeder json for bakpiatransaction

--- database/seeders/BakpiaTransactionSeeder.php
+++ database/seeders/BakpiaTransactionSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class BakpiaTransactionSeeder extends Seeder
 {
@@ -14,91 +15,22 @@ class BakpiaTransactionSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('bakpia_transactions')->insert([
-            [
-                'id_transaction' => 'BAK_20250101001',
-                'id_Customer' => '1',
-                'id_payment' => '1',
-                'id_outlet' => 'OUTLET_1',
-                'transaction_detail' => '[{"amount": "2", "id_bakpia": "1", "price_per": 40000, "box_varian": "8"}]',
-                'total_price' => '40000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now()->subDays(2),
-                'updated_at' => Carbon::now()->subDays(2),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101002',
-                'id_Customer' => '2',
-                'id_payment' => '1',
-                'id_outlet' => 'OUTLET_1',
-                'transaction_detail' => '[{"amount": "3", "id_bakpia": "1", "price_per": 60000, "box_varian": "8"}]',
-                'total_price' => '60000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now()->subDays(2),
-                'updated_at' => Carbon::now()->subDays(2),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101003',
-                'id_Customer' => '1',
-                'id_payment' => '1',
-                'id_outlet' => 'OUTLET_1',
-                'transaction_detail' => '[{"amount": "3", "id_bakpia": "1", "price_per": 60000, "box_varian": "8"}]',
-                'total_price' => '60000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now()->subDays(1),
-                'updated_at' => Carbon::now()->subDays(1),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101004',
-                'id_Customer' => '2',
-                'id_payment' => '1',
-                'id_outlet' => 'OUTLET_1',
-                'transaction_detail' => '[{"amount": "30", "id_bakpia": "3", "price_per": 600000, "box_varian": "8"}]',
-                'total_price' => '600000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101005',
-                'id_Customer' => '2',
-                'id_payment' => '2',
-                'id_outlet' => 'OUTLET_2',
-                'transaction_detail' => '[{"amount": "20", "id_bakpia": "2", "price_per": 400000, "box_varian": "8"}]',
-                'total_price' => '400000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101006',
-                'id_Customer' => '2',
-                'id_payment' => '3',
-                'id_outlet' => 'OUTLET_3',
-                'transaction_detail' => '[{"amount": "20", "id_bakpia": "2", "price_per": 400000, "box_varian": "8"}]',
-                'total_price' => '400000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'id_transaction' => 'BAK_20250101007',
-                'id_Customer' => '2',
-                'id_payment' => '2',
-                'id_outlet' => 'OUTLET_1',
-                'transaction_detail' => '[{"amount": "10", "id_bakpia": "2", "price_per": 200000, "box_varian": "8"}]',
-                'total_price' => '200000',
-                'discount' => '0',
-                'status' => 'PAID',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        ]);
+        $json = File::get(database_path('seeders/seeder_data/BakpiaTransactionDataSeeder.json'));
+        $transactions = json_decode($json);
+
+        foreach ($transactions as $transaction) {
+            DB::table('bakpia_transactions')->insert([
+                'id_transaction' => $transaction->id_transaction,
+                'id_Customer' => $transaction->id_Customer,
+                'id_payment' => $transaction->id_payment,
+                'id_outlet' => $transaction->id_outlet,
+                'transaction_detail' => $transaction->transaction_detail,
+                'total_price' => $transaction->total_price,
+                'discount' => $transaction->discount,
+                'status' => $transaction->status,
+                'created_at' => Carbon::parse($transaction->created_at),
+                'updated_at' => Carbon::parse($transaction->updated_at),
+            ]);
+        }
     }
 }
