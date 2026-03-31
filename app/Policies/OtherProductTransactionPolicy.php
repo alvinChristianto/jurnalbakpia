a081f3c6ff098658731dcb3e25f75a0d6c154ae1
alvinChristianto <alvinchristianto93@gmail.com>
2026-04-01 05:22:51 +0700

    feat: add policy to other product and other product transaction

---
+++ app/Policies/OtherProductTransactionPolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\OtherProductTransaction;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OtherProductTransactionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('view_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('update_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('delete_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('force_delete_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('restore_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, OtherProductTransaction $otherProductTransaction): bool
+    {
+        return $user->can('replicate_other::product::transaction');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_other::product::transaction');
+    }
+}
